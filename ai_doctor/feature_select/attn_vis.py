0eaf273f66ff4a6157055ba83971c4e3392266bc
hzm-January <hzm_dream@163.com>
2024-08-18 20:29:30 +0800

    visual attentions

--- ai_doctor/feature_select/attn_vis.py
+++ ai_doctor/feature_select/attn_vis.py
@@ -12,7 +12,9 @@ from loguru import logger
 from transformers.generation import GenerationConfig
 from matplotlib.font_manager import FontProperties
 from matplotlib import font_manager
-from bertviz import model_view
+
+import seaborn as sns
+from matplotlib.colors import LogNorm
 
 column_name_json = '/public/whr/hzm/code/qwen2/ai_doctor/source/patient_infos_column_name_0_1_2.json'
 
@@ -119,62 +121,154 @@ def calc_attn_one_model(args, dir_id):
             output_scores=True,
         )
 
-        output_text = tokenizer.decode(output['sequences'][0], skip_special_tokens=True)
-        logger.info(f'output_text: {output_text}')
+        input_ids_tensor = model_inputs["input_ids"]
+        input_ids_len = input_ids_tensor.shape[1]
+        input_ids = input_ids_tensor[0].tolist()
 
-        inputs = tokenizer(output_text, return_tensors="pt").to(cuda)
-        out = model(**inputs, output_attentions=True)
-        attention = out['attentions']
-        logger.info(f'attention shape: {attention[0].shape}')
-        tokens = tokenizer.convert_ids_to_tokens(inputs['input_ids'][0])
-        logger.info(f'tokens: {tokens}')
-        model_view(attention, tokens)
+        attentions = output["attentions"]
+        attentions = torch.tensor(attentions[-1]).to(cuda)
+        attention_layers = torch.mean(attentions, axis=2).float()
+        layer_num, bs, head_num, s1, s2 = attention_layers.shape
+        attention_layers = attention_layers.reshape(layer_num, s1, s2)
+        output_token_decode_str = tokenizer.decode(output["sequences"][0], skip_special_tokens=True)
+
+        answer = output_token_decode_str[len(content) - 1:]
+
+        # layer_num = attention_layers.shape[0]
+
+        attn_map_layer = []
+
+        for l in range(layer_num):
+
+            attention = attention_layers[l]
+
+            attn_map = {}
+
+            for i, column_name in enumerate(column_names):
+                if 'Stress' in column_name: continue  # TODO: 待修复，Stress-strain 匹配不到
+                cur_strs = re.findall(fr'({re.escape(column_name)}.*?),', content)
+                # logger.info(f'{i}----{column_name}-----{cur_strs}')
+                if len(cur_strs) == 0: continue
+                cur_str = cur_strs[0]
+                cur_str_tokens = tokenizer(cur_str, return_tensors='pt').to(cuda)
+                cur_str_ids = cur_str_tokens["input_ids"][0].tolist()
+                # logger.info(f'----cur_str_ids: {cur_str_ids}')
+                cur_str_ids_len = len(cur_str_ids)
+                start_i = -1
+                for j in range(input_ids_len - cur_str_ids_len + 1):
+                    if input_ids[j:j + cur_str_ids_len] == cur_str_ids:
+                        start_i = j
+                        break
+
+                # TODO: 待解决，B,H等开头的英文字母会和前面的逗号一起作为token
+                if start_i == -1:
+                    cur_str_tokens = tokenizer(',' + cur_str, return_tensors='pt').to(cuda)
+                    cur_str_ids = cur_str_tokens["input_ids"][0].tolist()
+                    # logger.info(f'----cur_str_ids: {cur_str_ids}')
+                    cur_str_ids_len = len(cur_str_ids)
+                    for j in range(input_ids_len - cur_str_ids_len + 1):
+                        if input_ids[j:j + cur_str_ids_len] == cur_str_ids:
+                            start_i = j
+                            break
+                # TODO: 待解决，B,H等开头的英文字母会和前面的逗号一起作为token
+                if start_i == -1:
+                    cur_str_tokens = tokenizer(' ' + cur_str, return_tensors='pt').to(cuda)
+                    cur_str_ids = cur_str_tokens["input_ids"][0].tolist()
+                    # logger.info(f'----cur_str_ids: {cur_str_ids}')
+                    cur_str_ids_len = len(cur_str_ids)
+                    for j in range(input_ids_len - cur_str_ids_len + 1):
+                        if input_ids[j:j + cur_str_ids_len] == cur_str_ids:
+                            start_i = j
+                            break
+                # print(start_i)
+                attn = attention[-1][start_i: start_i + cur_str_ids_len]
+                # logger.info(f'-------- attn: {attn}')
+                attn_sum = torch.sum(attn).item()
+                attn_map[column_name] = attn_sum
+
+            attn_map_layer.append(list(attn_map.values()))
+            ## rank attentions
+            # sorted_items = sorted(attn_map.items(), key=lambda item: item[1], reverse=False)
+            # sorted_attn_map = {key: value for key, value in sorted_items}
+            # # logger.info(f'-------- attention: {sorted_attn_map}')
+            #
+            # # keys, values = zip(*sorted_items[:30])
+            # keys, values = zip(*sorted_items)
+            #
+            # # show_attn_rank(keys, values)
+
+        attns_for_columns = torch.tensor(attn_map_layer).to(cuda)
+
+        layer = 0
+        # visualize_attention(attentions[-1][layer],
+        #                     output_path=args.path["dataset_dir"] + f"/attn_maps/atten_map_{layer}.png",
+        #                     title=f"layer {layer}")
+
+        visualize_attention(attns_for_columns,
+                            output_path=args.path["dataset_dir"] + f"/attn_maps/atten_map_{layer}.png",
+                            title=f"layer {layer}")
+
         break
-        #
-        # input_ids_tensor = model_inputs["input_ids"]
-        # input_ids_len = input_ids_tensor.shape[1]
-        # input_ids = input_ids_tensor[0].tolist()
-        #
-        # # logger.info(f'input_ids: {input_ids}')
-        #
-        # # stop_words_ids = [tokenizer.encode("Human:"), [tokenizer.eod_id]]
-        #
-        # # output = model.generate(
-        # #     **model_inputs,
-        # #     # stop_words_ids=stop_words_ids,
-        # #     generation_config=generation_config,
-        # #     # max_new_tokens=100,
-        # #     return_dict_in_generate=True,
-        # #     output_attentions=True,
-        # #     output_scores=True,
-        # # )
-        # # logger.info(f'-------- output type: {type(output)}')
-        # # logger.info(f'-------- output sequences type: {type(output["sequences"])}')
-        # # logger.info(f'-------- output sequences shape: {output["sequences"].shape}')
-        # # logger.info(f'-------- output sequences[0] shape: {output["sequences"][0].shape}')
-        # # logger.info(f'-------- output sequences[0]: {output["sequences"][0]}')
-        # attentions = output["attentions"]
-        # # logger.info(f'-------- output attention type :{attentions[-1][-1]}')
-        # # logger.info(f'-------- output attention type :{type(attentions[-1][-1])}')
-        # # logger.info(f'-------- output attention <0 count: {(attentions[-1][-1]<0).sum().item()}')
-        #
-        # # logger.info(f'-------- output attention length: {len(attentions)}')
-        # # logger.info(f'-------- output attention [-1]: {attentions[-1]}')
-        # # logger.info(f'-------- output attention [-1] length: {len(attentions[-1])}')  # 取输出的最后一个token对应的attentions
-        # # logger.info(f'-------- output attention [-1][-1]: {attentions[-1][-1]}')  # 取输出的最后一个token对应的最后一层attention
-        # # logger.info(
-        # #     f'-------- output attention [-1][-1] shape: {attentions[-1][-1].shape}')  # 取输出的最后一个token对应的最后一层attention
-        #
-        # # tokens = tokenizer.tokenize(text)
-        # tokens = tokenizer.convert_ids_to_tokens(input_ids_tensor[0])
-        # logger.info(f'tokens: {tokens}')
-        #
-        # logger.info(f'attentions[-1] len: {len(attentions[-1])}')
-        # logger.info(f'attentions[-1] shape: {attentions[-1][0].shape}')
-        # logger.info(f'tokens: {tokens}')
-        # model_view(attentions[-1], tokens, include_layers=[1, -1], include_heads=[0, -1])
 
     return attn_map_f
+
+
+def visualize_attention(averaged_attention, output_path="atten_map_1.png", title="Layer 5"):
+    # Assuming the input is a numpy array of shape (1, num_heads, n_tokens, n_tokens)
+    # First, we average the attention scores over the multiple heads
+    # averaged_attention = torch.mean(multihead_attention, axis=1)[0].float()  # Shape: (n_tokens, n_tokens)
+
+    # pooling the attention scores  with stride 20
+    # averaged_attention = torch.nn.functional.avg_pool2d(averaged_attention.unsqueeze(0).unsqueeze(0), 20,
+    #                                                     stride=20).squeeze(0).squeeze(0)
+
+    cmap = plt.colormaps.get_cmap("viridis")
+    plt.figure(figsize=(5, 5), dpi=400)
+
+    # Log normalization
+    log_norm = LogNorm(vmin=0.0007, vmax=averaged_attention.max())
+
+    # set the x and y ticks to 20x of the original
+
+    ax = sns.heatmap(averaged_attention.cpu(),
+                     cmap=cmap,  # custom color map
+                     norm=log_norm,  #
+                     # cbar_kws={'label': 'Attention score'},
+                     )
+
+    # remove the x and y ticks
+
+    # replace the x and y ticks with string
+
+    x_ticks = [str(i * 20) for i in range(0, averaged_attention.shape[1])]
+    y_ticks = [str(i * 20) for i in range(0, averaged_attention.shape[0])]
+    ax.set_xticks([i for i in range(0, averaged_attention.shape[1])])
+    ax.set_yticks([i for i in range(0, averaged_attention.shape[0])])
+    ax.set_xticklabels(x_ticks)
+    ax.set_yticklabels(y_ticks)
+
+    # change the x tinks font size
+    plt.xticks(fontsize=3)
+    plt.yticks(fontsize=3)
+
+    # make y label vertical
+    plt.yticks(rotation=0)
+    plt.xticks(rotation=90)
+
+    plt.title(title)
+    # tight layout
+    plt.savefig(output_path, bbox_inches='tight')
+    # plt.show()
+
+    top_five_attentions = []
+    for row in averaged_attention:
+        # Use torch.topk to get the top 5 values and their indices
+        top_values, top_indices = torch.topk(row, 10)
+        # Convert to lists and append to the overall list
+        top_five_line = list(zip(top_indices.tolist(), top_values.tolist()))
+        top_five_attentions.append(top_five_line)
+
+    return top_five_attentions, averaged_attention
 
 
 if __name__ == '__main__':
